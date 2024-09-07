81c0d1caedbcf3872bb0e23d50f01be2abbfb94a
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2024-09-07 22:39:00 +0530

    user management

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,48 +1,4 @@
-// import HeaderDash from "../components/HeaderDash";
-// import PropTypes from "prop-types";
-
-// const Profile = ({ user }) => {
-//   return (
-//     <>
-//       <HeaderDash />
-//       <div className="p-6 mx-4 bg-white rounded-lg shadow-md max-w-screen">
-//         <h2 className="mb-4 text-2xl font-bold">User Profile</h2>
-//         <div className="mb-4">
-//           <span className="font-semibold">Name:</span>
-//           <p>{user.name}</p>
-//         </div>
-//         <div className="mb-4">
-//           <span className="font-semibold">Email:</span>
-//           <p>{user.email}</p>
-//         </div>
-//         <div className="mb-4">
-//           <span className="font-semibold">Last Logged In:</span>
-//           <p>{new Date(user.lastLoggedIn).toLocaleString()}</p>
-//         </div>
-//         <div className="mb-4">
-//           <span className="font-semibold">Designation:</span>
-//           <p>{user.designation}</p>
-//         </div>
-//         <div className="mb-4">
-//           <span className="font-semibold">Gender:</span>
-//           <p>{user.gender}</p>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// Profile.propTypes = {
-//   user: PropTypes.shape({
-//     name: PropTypes.string,
-//     email: PropTypes.string,
-//     lastLoggedIn: PropTypes.string,
-//     designation: PropTypes.string,
-//     gender: PropTypes.string,
-//   }),
-// };
-
-// export default Profile;
+import HeaderDash from "../components/HeaderDash";
 
 import { useEffect, useState } from "react";
 import supabase from "../config/SupabaseClient";
@@ -92,61 +48,66 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      {loading ? (
-        <p>Loading...</p>
-      ) : errorMessage ? (
-        <p className="text-red-500">{errorMessage}</p>
-      ) : (
-        profile && (
-          <div className="w-full max-w-md p-6 space-y-4 bg-white rounded-lg shadow-lg">
-            <h2 className="text-2xl font-semibold text-center">Your Profile</h2>
-            <div>
-              <label className="block font-medium text-gray-700">Email</label>
-              <p className="p-2 mt-1 border border-gray-300 rounded-lg">
-                {profile.email}
-              </p>
-            </div>
+    <>
+      <HeaderDash></HeaderDash>
+      <div className="flex items-center justify-center h-screen">
+        {loading ? (
+          <p>Loading...</p>
+        ) : errorMessage ? (
+          <p className="text-red-500">{errorMessage}</p>
+        ) : (
+          profile && (
+            <div className="w-full max-w-md p-6 space-y-4 bg-white rounded-lg shadow-lg">
+              <h2 className="text-2xl font-semibold text-center">
+                Your Profile
+              </h2>
+              <div>
+                <label className="block font-medium text-gray-700">Email</label>
+                <p className="p-2 mt-1 border border-gray-300 rounded-lg">
+                  {profile.email}
+                </p>
+              </div>
 
-            <div>
-              <label className="block font-medium text-gray-700">
-                First Name
-              </label>
-              <p className="p-2 mt-1 border border-gray-300 rounded-lg">
-                {profile.first_name}
-              </p>
-            </div>
+              <div>
+                <label className="block font-medium text-gray-700">
+                  First Name
+                </label>
+                <p className="p-2 mt-1 border border-gray-300 rounded-lg">
+                  {profile.first_name}
+                </p>
+              </div>
 
-            <div>
-              <label className="block font-medium text-gray-700">
-                Last Name
-              </label>
-              <p className="p-2 mt-1 border border-gray-300 rounded-lg">
-                {profile.last_name}
-              </p>
-            </div>
+              <div>
+                <label className="block font-medium text-gray-700">
+                  Last Name
+                </label>
+                <p className="p-2 mt-1 border border-gray-300 rounded-lg">
+                  {profile.last_name}
+                </p>
+              </div>
 
-            <div>
-              <label className="block font-medium text-gray-700">
-                Badge Number
-              </label>
-              <p className="p-2 mt-1 border border-gray-300 rounded-lg">
-                {profile.badge_number}
-              </p>
-            </div>
+              <div>
+                <label className="block font-medium text-gray-700">
+                  Badge Number
+                </label>
+                <p className="p-2 mt-1 border border-gray-300 rounded-lg">
+                  {profile.badge_number}
+                </p>
+              </div>
 
-            <div>
-              <label className="block font-medium text-gray-700">
-                Created At
-              </label>
-              <p className="p-2 mt-1 border border-gray-300 rounded-lg">
-                {new Date(profile.created_at).toLocaleDateString()}
-              </p>
+              <div>
+                <label className="block font-medium text-gray-700">
+                  Created At
+                </label>
+                <p className="p-2 mt-1 border border-gray-300 rounded-lg">
+                  {new Date(profile.created_at).toLocaleDateString()}
+                </p>
+              </div>
             </div>
-          </div>
-        )
-      )}
-    </div>
+          )
+        )}
+      </div>
+    </>
   );
 };
 
