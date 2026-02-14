fcd0a624a0e44381c2eb93b4707bcf6899b8a5b4
Govardhan A R <govardhanar06@gmail.com>
2026-02-14 15:52:56 +0530

    final changes: 14/2/26 archive

--- backend/src/gender_classifier/model.py
+++ backend/src/gender_classifier/model.py
@@ -57,15 +57,14 @@ class SpatialTransformBlock(nn.Module):
     def stn(self, x, theta):
         grid = F.affine_grid(theta, x.size())
         x = F.grid_sample(x, grid, padding_mode='border')
-        return x.cuda()
+        return x.to(x.device)
 
     def transform_theta(self, theta_i, region_idx):
-        theta = torch.zeros(theta_i.size(0), 2, 3)
+        theta = torch.zeros(theta_i.size(0), 2, 3, device=theta_i.device)
         theta[:,0,0] = torch.sigmoid(theta_i[:,0])
         theta[:,1,1] = torch.sigmoid(theta_i[:,1])
         theta[:,0,2] = torch.tanh(theta_i[:,2])
         theta[:,1,2] = torch.tanh(theta_i[:,3])
-        theta = theta.cuda()
         return theta
 
     def forward(self, features):
